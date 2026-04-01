88dbccab26410fec923c11494093425fce7eab87
CP <1575042094@qq.com>
2026-04-01 21:05:36 +0800

    1

--- env.sh
+++ env.sh
@@ -5,7 +5,7 @@ export TASK_NAME=bi_so101_handover_cube
 export TASK_TEXT="Pick up the cube with the left arm and hand it to the right arm."
 
 
-export tworemotesuccess=tworemoteuccess1
+export startcaiji=startcaiji1
 
 export LOCAL_DATA_ROOT=/home/jy/Data/YCP/lerobot_datasets
 export SERVER_DATA_ROOT=/home/jy/Data/a100_lerobot_datasets
